a3f1f91e3a663e5f7832d145127905989ca3cfb8
Vitaliy Dyachuk <shadespiritenator@gmail.com>
2026-05-04 15:45:20 +0300

    Fix GitHub Pages asset base path

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,7 @@ import react from "@vitejs/plugin-react";
 import { fileURLToPath, URL } from "node:url";
 
 export default defineConfig({
+  base: "/javascript-beginners-2026/",
   plugins: [react()],
   resolve: {
     alias: {
