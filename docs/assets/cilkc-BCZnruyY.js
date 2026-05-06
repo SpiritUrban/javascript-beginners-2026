fe4aa859352e872476d14c5715b052b9c5148c37
Vitaliy Dyachuk <shadespiritenator@gmail.com>
2026-05-06 20:24:34 +0300

    Add debugging lesson

---
+++ docs/assets/cilkc-BCZnruyY.js
@@ -0,0 +1,1 @@
+import e from"./c-CooxPoq2.js";import"./index-C7U16Pkf.js";c.displayName="cilkc";c.aliases=["cilk-c"];function c(a){a.register(e),a.languages.cilkc=a.languages.insertBefore("c","function",{"parallel-keyword":{pattern:/\bcilk_(?:for|reducer|s(?:cope|pawn|ync))\b/,alias:"keyword"}}),a.languages["cilk-c"]=a.languages.cilkc}export{c as default};
