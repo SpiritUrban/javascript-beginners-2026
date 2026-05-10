24a50d11e7c5efe85b59fb9e4266b4b4a397a2eb
Vitaliy Dyachuk <shadespiritenator@gmail.com>
2026-05-10 12:08:35 +0300

    Update lesson 21 to 24 video links

---
+++ docs/assets/t4-cs-3_LLrjfa.js
@@ -0,0 +1,1 @@
+import e from"./csharp-CkJD4IaT.js";import g from"./t4-templating-DXZYDXwi.js";import"./index-D_YP_FAd.js";a.displayName="t4-cs";a.aliases=["t4"];function a(t){t.register(e),t.register(g),t.languages.t4=t.languages["t4-cs"]=t.languages["t4-templating"].createT4("csharp")}export{a as default};
